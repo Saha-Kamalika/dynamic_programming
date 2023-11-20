4e897ff63dce55e35a33690d0bb0c3d02fd3672d
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-11-20 09:38:19 +0530

    Update IV) v) Egg Dropping.cpp

--- IV) v) Egg Dropping.cpp
+++ IV) v) Egg Dropping.cpp
@@ -1,22 +1,25 @@
-class Solution
-{
-    public:
-    int t[201][201];
+class Solution {
+public:
+    int t[101][10001];
     int solve(int e,int f){
         if(f==1 || f==0) return f;
         if(e==1) return f;
         if(t[e][f]!=-1) return t[e][f];
-
+        int low=1,high=f;
         int mn=INT_MAX;
-        for(int k=1;k<=f;k++){
-            int temp=1+max(solve(e-1,k-1),solve(e,f-k));
+        while(low<=high){
+            int mid=(low+high)/2;
+            int left=solve(e-1,mid-1);
+            int right=solve(e,f-mid);
+            int temp=1+max(left,right);
+            if(left<right) low=mid+1;
+            else high=mid-1;
             mn=min(mn,temp);
         }
         return t[e][f]=mn;
     }
-    int eggDrop(int n, int k) 
-    {
+    int superEggDrop(int k, int n) {
         memset(t,-1,sizeof(t));
-        return solve(n,k);
+        return solve(k,n);
     }
 };
